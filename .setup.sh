6a9d6f835df2ef662d67817c603c995bd88bfbd3
Adrien Gallouët <adrien@gallouet.fr>
2019-09-19 15:42:24 +0200

    Update .setup
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .setup.sh
+++ .setup.sh
@@ -1,17 +1,21 @@
 git clone https://github.com/SchedMD/slurm
 cd slurm
 
+prefix=/tmp
+
 ./autogen.sh
-./configure --disable-dependency-tracking --prefix=/tmp
-make install
+./configure --disable-dependency-tracking --prefix=$prefix
+make install -j
+
+[ "$PKG_CONFIG_PATH" ] || PKG_CONFIG_PATH=$prefix/lib/pkgconfig
 
 mkdir -p ${PKG_CONFIG_PATH}
 cat >${PKG_CONFIG_PATH}/slurm.pc <<EOF
-includedir=/tmp/include
-libdir=/tmp/lib
+includedir=$prefix/include
+libdir=$prefix/lib
 Cflags: -I\${includedir}
 Libs: -L\${libdir} -lslurm
 Description: Slurm API
-$(grep Name META)
-$(grep Version META)
+Name: slurm
+Version: $(grep Version META | cut -f2)
 EOF
